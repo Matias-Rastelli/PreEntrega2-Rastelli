4caadefb82e78d9787c01a098d72b516184b5daf
Matias Rastelli <oxaservice@gmail.com>
2023-05-24 23:34:49 -0300

    agregados botones tipo pokemon

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, Button, Box, Typography } from "@mui/material"
+import { ButtonGroup, Button, Box, Typography, Stack } from "@mui/material"
 import { CartWidget } from "../../common/cartWidget/CartWidget"
 
 export const NavBar = () => {
@@ -18,20 +18,56 @@ export const NavBar = () => {
     >
       <Typography
         variant="h4"
+        align="center"
         sx={{
           fontWeight: 600,
           letterSpacing: ".2rem",
           fontFamily: "monospace",
         }}
       >
-        NOMBRE EMPRESA
+        PokeTienda
       </Typography>
-      <ButtonGroup variant="contained" color="primary" size="large">
-        <Button>Categoria 1</Button>
-        <Button>Categoria 2</Button>
-        <Button>Categoria 3</Button>
-        <Button>Categoria 4</Button>
-      </ButtonGroup>
+      <Stack spacing={1}>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth="true"
+        >
+          <Button>Acero</Button>
+          <Button>Agua</Button>
+          <Button>Bicho</Button>
+          <Button>Dragón</Button>
+          <Button>Eléctrico</Button>
+          <Button>Fantasma</Button>
+        </ButtonGroup>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth="true"
+        >
+          <Button>Fuego</Button>
+          <Button>Hada</Button>
+          <Button>Hielo</Button>
+          <Button>Lucha</Button>
+          <Button>Normal</Button>
+          <Button>Planta</Button>
+        </ButtonGroup>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth="true"
+        >
+          <Button>Psíquico</Button>
+          <Button>Roca</Button>
+          <Button>Suministro</Button>
+          <Button>Tierra</Button>
+          <Button>Veneno</Button>
+          <Button>Volador</Button>
+        </ButtonGroup>
+      </Stack>
       <CartWidget />
     </Box>
   )
